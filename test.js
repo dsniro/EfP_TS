a843674f9f444702faa1a01096d0e012301148c0
dsniro <derek.niro@gmail.com>
2023-07-18 19:27:34 -0500

    First commit

---
+++ test.js
@@ -0,0 +1,8 @@
+"use strict";
+let num1 = 5;
+let num2 = 10;
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log("Result: ");
+console.log(add(num1, num2));
---
+++ test.ts
@@ -0,0 +1,9 @@
+let num1: number = 5;
+let num2: number = 10;
+
+function add(num1: number, num2: number) : number {
+    return num1+ num2;
+}
+
+console.log("Result: ");
+console.log(add(num1, num2));
